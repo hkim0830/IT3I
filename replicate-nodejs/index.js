c46cdb657bc0df00c1449afda5de77f4eebaa5e7
Hosanna Kim <hosannakim@Hosannas-MacBook-Pro.local>
2023-07-20 22:49:03 -0700

    Started Layout

--- replicate-nodejs/index.js
+++ replicate-nodejs/index.js
@@ -5,7 +5,7 @@ const replicate = new Replicate({
 });
 
 const model = "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf";
-const input = { prompt: "an astronaut riding a horse on mars, hd, dramatic lighting, detailed" };
+const input = { prompt: "Wolf with dark blue nightsky in background, detailed, hd " };
 const output = await replicate.run(model, { input });
 
 console.log(output);
