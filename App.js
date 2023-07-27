8c54f368d4b35b5d9e9cfc93119acc89df091bb0
s-almeda <shm.almeda@berkeley.edu>
2023-07-27 03:48:18 -0700

    added Firebase and multiple images processing support

--- App.js
+++ App.js
@@ -14,6 +14,26 @@ Bunny by Satyabratasm on Unsplash <https://unsplash.com/photos/u_kMWN-BWyU>
 
 //Node modules to *require*
 //if these cause errors, be sure you've installed them, ex: 'npm install express'
+
+const { getStorage, ref, uploadString, getDownloadURL } = require('firebase/storage');
+const  { initializeApp } = require('firebase/app');
+const firebaseConfig = {
+  apiKey: "AIzaSyAwPFPdZk674g7Bf_SEKSsPiCvw1sOS4Q0",
+  authDomain: "it3i-4bae8.firebaseapp.com",
+  projectId: "it3i-4bae8",
+  storageBucket: "it3i-4bae8.appspot.com",
+  messagingSenderId: "702142588007",
+  appId: "1:702142588007:web:335777b7bbca521a4ee0c2",
+  storageBucket: "it3i-4bae8.appspot.com"
+};
+
+
+const db = initializeApp(firebaseConfig);
+const storage = getStorage(db);
+
+const serviceAccount = require('./it3i-394100-e431fabfb3d8.json');
+
+
 const express = require('express');
 const router = express.Router();
 const app = express();
@@ -28,8 +48,8 @@ const port = 8000;
 var publicPath = path.join(__dirname, 'public'); //get the path to use our "public" folder where we stored our html, css, images, etc
 app.use(express.static(publicPath));  //tell express to use that folder
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit:'50mb', extended: true }));
 
 //here's where we specify what to send to users that connect to our web server...
 //if there's no url extension, it will show "index.html"
@@ -68,6 +88,32 @@ app.post('/tokengenrequest', (req, res) =>{
         res.status(200).send(response);
     })
 });
+
+
+// Firestore handlers 
+app.post('/imageUpload', async (req,res)=>{
+    var fileName = req.body.name;
+    var imageRef = ref(storage, fileName);
+    var imageStringRaw = req.body.image;
+
+    //from Nicolo on Stack Overflow, ty https://stackoverflow.com/questions/38633061/how-can-i-strip-the-dataimage-part-from-a-base64-string-of-any-image-type-in-ja
+    var imageString = imageStringRaw.replace(/^data:image\/[a-z]+;base64,/, "");
+
+    //console.log(imageString);
+    uploadString(imageRef, imageString, 'base64').then((snapshot) => {
+      console.log('Uploaded a base64 string!');
+        getDownloadURL(imageRef).then((url) =>{
+            imageToText(url).then(function(response){
+                console.log('Success! SERVER received the following tokens from replicate, and is sending to user: ' + response + '\n');
+                res.status(200).send(response);
+            });
+
+        });
+    });
+
+
+})
+
 
 //run this server by entering "node App.js" using your command line. 
    app.listen(port, () => {
--- replicateITT.js
+++ replicateITT.js
@@ -5,7 +5,7 @@ const rep = new Replicate({
 module.exports = async function (userImage){
   console.log("replicate module has received image");
   const model = "pharmapsychotic/clip-interrogator:a4a8bafd6089e1716b06057c42b19378250d008b80fe87caa5cd36d40c1eda90";
-  const input = {image: `${userImage}`, mode : 'fast'   };
+  const input = {image: `${userImage}`, clip_model_name : 'ViT-H-14/laion2b_s32b_b79k', mode : 'fast'   };
   const output = await rep.run(model, { input });
   console.log("replicate module has finished running, and is sending: " + output + "\n");
   return output;
